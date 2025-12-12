26cb819ffe580d52eb7599c6922d0e93699cb4f3
Eric-chagas <eric_chagas12@outlook.com>
2025-12-11 22:20:23 -0300

    black formatting

--- project/core/api/views/oauth_google.py
+++ project/core/api/views/oauth_google.py
@@ -1,8 +1,10 @@
 from django.views.decorators.http import require_GET
 
+
 @require_GET
 def google_login(request):
     raise NotImplementedError("Implementar na Etapa 2")
+
 
 @require_GET
 def google_callback(request):
