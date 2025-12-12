6be69aa71569a9397517ffadb3d5aca40a117810
Lude Ribeiro <ludeyuri07@gmail.com>
2025-12-12 16:11:11 -0300

    fix: remove default secret key value for enhanced security

--- project/project/settings.py
+++ project/project/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env(BASE_DIR.parent / ".env")
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY", default="django-insecure-change-me-in-production")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
