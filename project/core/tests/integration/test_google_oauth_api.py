020c718d79be705ce42aa02644af79202fd5d806
Eric-chagas <eric_chagas12@outlook.com>
2025-12-12 12:22:20 -0300

    removing unnecessary declarations

--- project/core/tests/integration/test_google_oauth_api.py
+++ project/core/tests/integration/test_google_oauth_api.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
 
 
 class ProfilePageTest(TestCase):
@@ -11,9 +10,6 @@ class ProfilePageTest(TestCase):
 
 class ProfilePageLoggedTest(TestCase):
     def test_profile_access_logged(self):
-        User = get_user_model()
-        user = User.objects.create_user(email="test@example.com", password="1234")
-
         self.client.login(email="test@example.com", password="1234")
         response = self.client.get("/profile/")
         self.assertEqual(response.status_code, 200)
