c8263568a1436975e18640828a54c2da75864154
Eric-chagas <eric_chagas12@outlook.com>
2025-12-11 22:19:54 -0300

    adding require get to supress sonar hotspot warning

--- project/core/api/views/oauth_google.py
+++ project/core/api/views/oauth_google.py
@@ -1,6 +1,9 @@
+from django.views.decorators.http import require_GET
+
+@require_GET
 def google_login(request):
     raise NotImplementedError("Implementar na Etapa 2")
 
-
+@require_GET
 def google_callback(request):
     raise NotImplementedError("Implementar na Etapa 2")
